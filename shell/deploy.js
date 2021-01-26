a38b9e1ede9643ea6dc8633f250ba4e7f9c7374c
Xuedong Wang <xuedongwang@tencent.com>
2021-01-26 09:57:49 +0800

    commit

--- shell/deploy.js
+++ shell/deploy.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const OSS = require('ali-oss');
 const argv = require('minimist')(process.argv.slice(2));
-console.log(process.env)
-console.log(argv)
+console.log(1212, process.env.bucket)
 const FILE_DIR = path.join(__dirname, '../public');
 
 let client = new OSS({
